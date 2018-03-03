a8311d73d495be73c863f599f20e030a86769ff1
Gabriel Garcia <gabrielgsdev@gmail.com>
2018-03-02 23:35:34 -0600

    Add ingredients adde

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,13 @@ import React, { Fragment, Component } from 'react'
 import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.4,
+  bacon: 0.7
+}
+
 class BurgerBuilder extends Component {
   // constructor(props) {
   //   super(props)
@@ -15,14 +22,31 @@ class BurgerBuilder extends Component {
       bacon: 0,
       cheese: 0,
       meat: 0
+    },
+    totalPrice: 4
+
+  }
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type]
+    const updateCount = oldCount + 1
+    const updatedIngredients = {
+      ...this.state.ingredients
     }
+    updatedIngredients[type] = updateCount
+    const priceAddition = INGREDIENT_PRICES[type]
+    const oldPrice = this.state.totalPrice
+    const newPrice = oldPrice + priceAddition
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+
   }
 
   render () {
     return (
       <Fragment>
         <Burger ingredients={this.state.ingredients}/>
-        <BuildControls />
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}/>
       </Fragment>
     )
   }
