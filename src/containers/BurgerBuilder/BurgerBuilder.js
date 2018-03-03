cc78def9e5a8e18e432f5ef9f230ada0b6208c13
Gabriel Garcia <gabrielgsdev@gmail.com>
2018-03-02 22:58:14 -0600

    Outputting multiple build controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,6 @@
 import React, { Fragment, Component } from 'react'
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
 class BurgerBuilder extends Component {
   // constructor(props) {
@@ -21,7 +22,7 @@ class BurgerBuilder extends Component {
     return (
       <Fragment>
         <Burger ingredients={this.state.ingredients}/>
-        <div>Build Controls</div>
+        <BuildControls />
       </Fragment>
     )
   }
