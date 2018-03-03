1d751787423fc6a2cc92dd95813ac85317d24d86
Gabriel Garcia <gabrielgsdev@gmail.com>
2018-03-02 18:06:32 -0600

    [ADD] implementation of the Burger Builder container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React, { Fragment } from 'react'
 import Aux from '../../hoc/Aux'
+import classes from './Layout.css'
 
 const layout = ( props ) => (
   <Fragment>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
+    <main className={classes.Content}>
       {props.children}
     </main>
   </Fragment>
