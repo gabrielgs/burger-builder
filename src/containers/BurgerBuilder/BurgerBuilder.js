f0a37a2a920fea646e155df5678d866d562ac778
Gabriel Garcia <gabrielgsdev@gmail.com>
2018-03-07 16:31:45 -0600

    [ADD] new condition in modal for show the spinner

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,7 +3,7 @@ import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
-import Spinner from '../../components/UI/Spinner'
+import Spinner from '../../components/UI/Spinner/Spinner'
 import axios from '../../axios-order'
 
 const INGREDIENT_PRICES = {
@@ -88,7 +88,8 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    // alert('You Continue!')
+    this.setState( {loading: true} )
+
     const order = {
       ingredients: this.state.ingredients,
       price: this.state.totalPrice,
@@ -104,8 +105,12 @@ class BurgerBuilder extends Component {
       deliveryMethod: 'fastest'
     }
     axios.post('/orders.json', order)
-      .then(response => console.log(response))
-      .catch(error => console.log(error))
+      .then(response => {
+        this.setState( {loading: false, purchasing: false} )
+      })
+      .catch(error => {
+        this.setState( {loading: false, purchasing: false} )
+      })
   }
 
   render () {
