9b7b5448da886aa210079b36cb5c032e98670bd0
Gabriel Garcia <gabrielgsdev@gmail.com>
2018-03-21 14:04:11 -0600

    fetchin data to orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react'
 
 import Order from '../../components/Order/Order'
+import axios from '../../axios-order'
 
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true
+  }
+
+  componentDidMount () {
+    axios.get('/orders.json')
+      .then(res => {
+        const fetchOrders = []
+
+        for (let key in res.data) {
+          fetchOrders.push({
+            ...res.data[key],
+            id: key
+          })
+        }
+
+        console.log(fetchOrders)
+        this.setState({loading: false, orders: fetchOrders})
+      })
+      .catch(err => {
+        this.setState({loading: false})
+      })
+  }
+
   render () {
     return (
       <div>
