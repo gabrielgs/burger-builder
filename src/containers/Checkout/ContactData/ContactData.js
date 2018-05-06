47e5821b6d5b7a7445a133b14542e3e6df9a254b
Gabriel Garcia <gabrielgsdev@gmail.com>
2018-05-05 19:40:28 -0500

    Add some validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -10,117 +10,162 @@ class ContactData extends Component {
   state = {
     orderForm: {
       name: {
-        elementType: 'input',
+        elementType: "input",
         elementConfig: {
-          type: 'text',
-          placeholder: 'Your Name'
+          type: "text",
+          placeholder: "Your Name"
         },
-        value: ''
+        value: "",
+        validation: {
+          required: true
+        },
+        valid: false
       },
       street: {
-        elementType: 'input',
+        elementType: "input",
         elementConfig: {
-          type: 'text',
-          placeholder: 'Street'
+          type: "text",
+          placeholder: "Street"
         },
-        value: ''
+        value: "",
+        validation: {
+          required: true
+        },
+        valid: false
       },
       zipCode: {
-        elementType: 'input',
+        elementType: "input",
         elementConfig: {
-          type: 'text',
-          placeholder: 'ZIP Code'
+          type: "text",
+          placeholder: "ZIP Code"
         },
-        value: ''
+        value: "",
+        validation: {
+          required: true,
+          minLength: 5,
+          maxLength: 5
+        },
+        valid: false
       },
       country: {
-        elementType: 'input',
+        elementType: "input",
         elementConfig: {
-          type: 'text',
-          placeholder: 'Country'
+          type: "text",
+          placeholder: "Country"
         },
-        value: ''
+        value: "",
+        validation: {
+          required: true
+        },
+        valid: false
       },
       email: {
-        elementType: 'input',
+        elementType: "input",
         elementConfig: {
-          type: 'email',
-          placeholder: 'Email'
+          type: "email",
+          placeholder: "Email"
         },
-        value: ''
+        value: "",
+        validation: {
+          required: true
+        },
+        valid: false
       },
       deliveryMethod: {
-        elementType: 'select',
+        elementType: "select",
         elementConfig: {
           options: [
-            {value: 'fastest', displayValue: 'Fastest'},
-            {value: 'cheapest', displayValue: 'Cheapest'},
+            { value: "fastest", displayValue: "Fastest" },
+            { value: "cheapest", displayValue: "Cheapest" }
           ]
         },
         value: ''
       }
     },
     loading: false
-  }
+  };
 
-  orderHandler  = (event) => {
-    event.preventDefault()
+  orderHandler = event => {
+    event.preventDefault();
 
-    this.setState( {loading: true} )
+    this.setState({ loading: true });
 
-    const formData = {}
+    const formData = {};
 
     for (let formElementIdentifier in this.state.orderForm) {
-      formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
+      formData[formElementIdentifier] = this.state.orderForm[
+        formElementIdentifier
+      ].value;
     }
 
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
       orderData: formData
-    }
+    };
 
-    axios.post('/orders.json', order)
+    axios
+      .post("/orders.json", order)
       .then(response => {
-        this.setState( {loading: false} )
-        this.props.history.push('/')
+        this.setState({ loading: false });
+        this.props.history.push("/");
       })
       .catch(error => {
-        this.setState( {loading: false} )
-      })
-    console.log(this.props.ingredients)
+        this.setState({ loading: false });
+      });
+    console.log(this.props.ingredients);
+  };
+
+  checkValidity(value, rules) {
+    let isValid = true
+
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid
+    }
+
+    return isValid
   }
 
   inputChangedHandler = (event, inputIdentifier) => {
     // Clone the the orderForm object
     const updatedOrderForm = {
       ...this.state.orderForm
-    }
+    };
 
     // Also clone the orderForm[inputKey] that would be
     // name, email, country, ... object
     const updatedFormElement = {
       ...updatedOrderForm[inputIdentifier]
-    }
+    };
 
-    updatedFormElement.value = event.target.value
-    updatedOrderForm[inputIdentifier] = updatedFormElement
+    updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+
+    console.log(updatedFormElement);
 
     this.setState({
       orderForm: updatedOrderForm
-    })
+    });
+  };
 
-  }
-
-  render () {
-    const formElementsArray = []
+  render() {
+    const formElementsArray = [];
 
     // Turn orderForm object into array of objects
     for (let key in this.state.orderForm) {
       formElementsArray.push({
         id: key,
         config: this.state.orderForm[key]
-      })
+      });
     }
 
     let form = (
@@ -131,22 +176,22 @@ class ContactData extends Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
-            changed={(event) => this.inputChangedHandler(event, formElement.id)}
+            changed={event => this.inputChangedHandler(event, formElement.id)}
           />
         ))}
-        <Button btnType="Success" >ORDER</Button>
+        <Button btnType="Success">ORDER</Button>
       </form>
-    )
+    );
 
     if (this.state.loading) {
-      form = <Spinner />
+      form = <Spinner />;
     }
     return (
       <div className={classes.ContactData}>
         <h4>Enter your Contact Data</h4>
         {form}
       </div>
-    )
+    );
   }
 }
 
