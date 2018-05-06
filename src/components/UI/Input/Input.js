9283bf1b68381b2b3f976e155c2747aec6e318c6
Gabriel Garcia <gabrielgsdev@gmail.com>
2018-05-05 19:02:13 -0500

    Add user input handler

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -10,6 +10,7 @@ const input = (props) => {
         className={classes.InputElement}
         {...props.elementConfig}
         value={props.value}
+        onChange={props.changed}
       />
       break
     case ('textarea'):
@@ -17,13 +18,15 @@ const input = (props) => {
         className={classes.InputElement}
         {...props.elementConfig}
         value={props.value}
+        onChange={props.changed}
       />
       break
     case ('select'):
       inputElement = (
         <select
           className={classes.InputElement}
-          value={props.value}>
+          value={props.value}
+          onChange={props.changed}>
           {props.elementConfig.options.map(option => (
             <option key={option.value} value={option.value}>
               {option.displayValue}
@@ -37,6 +40,7 @@ const input = (props) => {
         className={classes.InputElement}
         {...props.elementConfig}
         value={props.value}
+        onChange={props.changed}
       />
   }
 
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -84,6 +84,27 @@ class ContactData extends Component {
     console.log(this.props.ingredients)
   }
 
+  inputChangedHandler = (event, inputIdentifier) => {
+    // Clone the the orderForm object
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    }
+
+    // Also clone the orderForm[inputKey] that would be
+    // name, email, country, ... object
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier]
+    }
+
+    updatedFormElement.value = event.target.value
+    updatedOrderForm[inputIdentifier] = updatedFormElement
+
+    this.setState({
+      orderForm: updatedOrderForm
+    })
+
+  }
+
   render () {
     const formElementsArray = []
 
@@ -103,6 +124,7 @@ class ContactData extends Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
+            changed={(event) => this.inputChangedHandler(event, formElement.id)}
           />
         ))}
         <Button btnType="Success" clicked={this.orderHandler} >ORDER</Button>
