80b585eb6b554e4144c04eace6133811fcf5447d
Gabriel Garcia <gabrielgsdev@gmail.com>
2018-05-05 20:12:07 -0500

    Add validation feedback for error

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -15,11 +15,12 @@ class ContactData extends Component {
           type: "text",
           placeholder: "Your Name"
         },
-        value: "",
+        value: '',
         validation: {
           required: true
         },
-        valid: false
+        valid: false,
+        touched: false
       },
       street: {
         elementType: "input",
@@ -27,11 +28,12 @@ class ContactData extends Component {
           type: "text",
           placeholder: "Street"
         },
-        value: "",
+        value: '',
         validation: {
           required: true
         },
-        valid: false
+        valid: false,
+        touched: false
       },
       zipCode: {
         elementType: "input",
@@ -39,13 +41,14 @@ class ContactData extends Component {
           type: "text",
           placeholder: "ZIP Code"
         },
-        value: "",
+        value: '',
         validation: {
           required: true,
           minLength: 5,
           maxLength: 5
         },
-        valid: false
+        valid: false,
+        touched: false
       },
       country: {
         elementType: "input",
@@ -53,11 +56,12 @@ class ContactData extends Component {
           type: "text",
           placeholder: "Country"
         },
-        value: "",
+        value: '',
         validation: {
           required: true
         },
-        valid: false
+        valid: false,
+        touched: false
       },
       email: {
         elementType: "input",
@@ -69,7 +73,8 @@ class ContactData extends Component {
         validation: {
           required: true
         },
-        valid: false
+        valid: false,
+        touched: false
       },
       deliveryMethod: {
         elementType: "select",
@@ -117,21 +122,21 @@ class ContactData extends Component {
   };
 
   checkValidity(value, rules) {
-    let isValid = true
+    let isValid = true;
 
     if (rules.required) {
-      isValid = value.trim() !== '' && isValid
+      isValid = value.trim() !== "" && isValid;
     }
 
     if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid
+      isValid = value.length >= rules.minLength && isValid;
     }
 
     if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid
+      isValid = value.length <= rules.maxLength && isValid;
     }
 
-    return isValid
+    return isValid;
   }
 
   inputChangedHandler = (event, inputIdentifier) => {
@@ -147,7 +152,11 @@ class ContactData extends Component {
     };
 
     updatedFormElement.value = event.target.value;
-    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
+    updatedFormElement.valid = this.checkValidity(
+      updatedFormElement.value,
+      updatedFormElement.validation
+    )
+    updatedFormElement.touched = true
     updatedOrderForm[inputIdentifier] = updatedFormElement;
 
     console.log(updatedFormElement);
@@ -177,6 +186,9 @@ class ContactData extends Component {
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
             changed={event => this.inputChangedHandler(event, formElement.id)}
+            invalid={!formElement.config.valid}
+            shouldValidate={formElement.config.validation}
+            touched={formElement.config.touched}
           />
         ))}
         <Button btnType="Success">ORDER</Button>
